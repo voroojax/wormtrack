73eac4c852a74d90ba18e742c9481ccbf279d3bc
abrods01@gmail.com <abrods01@gmail.com@f22ef368-c671-8c5d-310e-2b9d97a6f9a6>
2011-11-02 22:42:28 +0000

    some fixes

--- trunk/wormtrack/sendAlert.sh
+++ trunk/wormtrack/sendAlert.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-cat /root/incidents/$1.incident|mail -s 'Incident Alert Incident ID #:'$1 somee-mail@test.tst
+EMAIL_TO=email@somehost.tst
+INCIDENTS_PATH=/root/incidents
+
+cat $INCIDENTS_PATH/$1.incident|mail -s 'Incident Alert Incident ID #:'$1 $EMAIL_TO
